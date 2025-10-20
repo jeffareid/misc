1efcef10d4e01d9d5b1a7a7ac864bc6b0c146da7
Jeff Reid <jeffareid@cox.net>
2025-10-20 08:07:13 -0700

    LFSR erasure code k=15 p=5
    LFSR erasure code k=15 p=5

--- ersp5a.asm
+++ ersp5a.asm
@@ -15,7 +15,10 @@
 ;       rax, rcx, rdx, r8, r9, r10, r11 xmm0, xmm1, xmm2, xmm3, xmm4, xmm5 free
 ;                       x5       x4       x3       x2       x1     x0
 xenc    proc
-        prefetcht0      [rdx]
+        vmovdqa64       zmm27,zmm7              ;save
+        vmovdqa64       zmm26,zmm6
+        vmovdqu64       zmm9,[rcx+  0]          ;mpy by ce
+        vmovdqu64       zmm8,[rcx+ 64]          ;mpy by e6
         mov             rax,r8                  ;rax = r8*15-64
         shl             rax,4
         sub             rax,r8
@@ -24,11 +27,9 @@ xenc    proc
         shr             r9,6
         lea             r10,[r8+r8*2]           ;r10 = r8*3
         lea             r11,[r8+r8*4]           ;r11 = r8*5
-        vmovdqu64       zmm31,[rcx+  0]         ;mpy by ce
-        vmovdqu64       zmm30,[rcx+ 64]         ;mpy by e6
-        vmovdqu64       zmm29,zmm6              ;save zmm6
 
-enc0:   vpxorq          zmm4,zmm4,zmm4          ;zero regs
+enc0:   vmovdqa64       zmm7,[rdx]
+        vpxorq          zmm4,zmm4,zmm4          ;zero regs
         vpxorq          zmm3,zmm3,zmm3
         vpxorq          zmm2,zmm2,zmm2
         vpxorq          zmm1,zmm1,zmm1
@@ -38,47 +39,46 @@ enc0:   vpxorq          zmm4,zmm4,zmm4          ;zero regs
 
         rept 3                                  ;encode 3*5 = 15 rows
 
-        vpxorq          zmm4,zmm4,[rdx]         ;x0 ^= data
-        prefetcht0      [rdx+r8]
-        vgf2p8affineqb  zmm5,zmm4,zmm31,0       ;z5 = x0*ce
-        vgf2p8affineqb  zmm6,zmm4,zmm30,0       ;z6 = x0*e6
+        vpxorq          zmm4,zmm4,zmm7          ;x0 ^= data
+        vmovdqa64       zmm7,[rdx+r8]
+        vgf2p8affineqb  zmm5,zmm4,zmm9,0        ;z5 = x0*ce
+        vgf2p8affineqb  zmm6,zmm4,zmm8,0        ;z6 = x0*e6
         vpxorq          zmm3,zmm3,zmm5          ;x4 ^= z5
         vpxorq          zmm0,zmm0,zmm5          ;x1 ^= z5
         vpxorq          zmm2,zmm2,zmm6          ;x3 ^= z6
         vpxorq          zmm1,zmm1,zmm6          ;x2 ^= z6
 
-        vpxorq          zmm3,zmm3,[rdx+r8]      ;x0 ^= data
-        prefetcht0      [rdx+r8*2]
-        vgf2p8affineqb  zmm5,zmm3,zmm31,0       ;z5 = x0*ce
-        vgf2p8affineqb  zmm6,zmm3,zmm30,0       ;z6 = x0*e6
+        vpxorq          zmm3,zmm3,zmm7          ;x0 ^= data
+        vmovdqa64       zmm7,[rdx+r8*2]
+        vgf2p8affineqb  zmm5,zmm3,zmm9,0        ;z5 = x0*ce
+        vgf2p8affineqb  zmm6,zmm3,zmm8,0        ;z6 = x0*e6
         vpxorq          zmm2,zmm2,zmm5          ;x4 ^= z5
         vpxorq          zmm4,zmm4,zmm5          ;x1 ^= z5
         vpxorq          zmm1,zmm1,zmm6          ;x3 ^= z6
         vpxorq          zmm0,zmm0,zmm6          ;x2 ^= z6
 
-        vpxorq          zmm2,zmm2,[rdx+r8*2]    ;x0 ^= data
-        prefetcht0      [rdx+r10]
-        vgf2p8affineqb  zmm5,zmm2,zmm31,0       ;z5 = x0*ce
-        vgf2p8affineqb  zmm6,zmm2,zmm30,0       ;z6 = x0*e6
+        vpxorq          zmm2,zmm2,zmm7          ;x0 ^= data
+        vmovdqa64       zmm7,[rdx+r10]
+        vgf2p8affineqb  zmm5,zmm2,zmm9,0        ;z5 = x0*ce
+        vgf2p8affineqb  zmm6,zmm2,zmm8,0        ;z6 = x0*e6
         vpxorq          zmm1,zmm1,zmm5          ;x4 ^= z5
         vpxorq          zmm3,zmm3,zmm5          ;x1 ^= z5
         vpxorq          zmm0,zmm0,zmm6          ;x3 ^= z6
         vpxorq          zmm4,zmm4,zmm6          ;x2 ^= z6
 
-        vpxorq          zmm1,zmm1,[rdx+r10]     ;x0 ^= data
-        prefetcht0      [rdx+r8*4]
-        vgf2p8affineqb  zmm5,zmm1,zmm31,0       ;z5 = x0*ce
-        vgf2p8affineqb  zmm6,zmm1,zmm30,0       ;z6 = x0*e6
+        vpxorq          zmm1,zmm1,zmm7          ;x0 ^= data
+        vmovdqa64       zmm7,[rdx+r8*4]
+        vgf2p8affineqb  zmm5,zmm1,zmm9,0        ;z5 = x0*ce
+        vgf2p8affineqb  zmm6,zmm1,zmm8,0        ;z6 = x0*e6
         vpxorq          zmm0,zmm0,zmm5          ;x4 ^= z5
         vpxorq          zmm2,zmm2,zmm5          ;x1 ^= z5
         vpxorq          zmm4,zmm4,zmm6          ;x3 ^= z6
         vpxorq          zmm3,zmm3,zmm6          ;x2 ^= z6
 
-        vpxorq          zmm0,zmm0,[rdx+r8*4]    ;x0 ^= data
+        vpxorq          zmm0,zmm0,zmm7          ;x0 ^= data
         add             rdx,r11                 ;rdx += ncol*5
-        prefetcht0      [rdx]
-        vgf2p8affineqb  zmm5,zmm0,zmm31,0       ;z5 = x0*ce
-        vgf2p8affineqb  zmm6,zmm0,zmm30,0       ;z6 = x0*e6
+        vgf2p8affineqb  zmm5,zmm0,zmm9,0        ;z5 = x0*ce
+        vgf2p8affineqb  zmm6,zmm0,zmm8,0        ;z6 = x0*e6
         vpxorq          zmm4,zmm4,zmm5          ;x4 ^= z5
         vpxorq          zmm1,zmm1,zmm5          ;x1 ^= z5
         vpxorq          zmm3,zmm3,zmm6          ;x3 ^= z6
@@ -93,11 +93,11 @@ enc0:   vpxorq          zmm4,zmm4,zmm4          ;zero regs
         vmovdqa64       [rdx+r8*4],zmm0         ;par0 = x0
 
         sub             rdx,rax                 ;rdx = ptr to next 64 columns
-        prefetcht0      [rdx]
         dec             r9                      ;loop till all columns done
         jnz             enc0
 
-        vmovdqu64       zmm6,zmm29              ;restore zmm6
+        vmovdqa64       zmm6,zmm26              ;restore
+        vmovdqa64       zmm7,zmm27
         ret
 xenc    endp
         end
